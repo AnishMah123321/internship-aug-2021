f91eb0487e24efd9e8358d9b3728b8743835cb3b
Anish Maharjan <58387513+AnishMah123321@users.noreply.github.com>
2021-08-18 09:31:20 +0545

    fetch in html with bootstrap

--- fetch/moviesApi.js
+++ fetch/moviesApi.js
@@ -1,33 +1,43 @@
 import {fetchMovie} from './modules/fetch.js';
 
 export const showMovie = async() => {
+    //asd();
     let info = await fetchMovie()
-    let count = 0;
+    let count = 1;
+    let rowId='rowId1';
+    let colId='';
+    let rowCount=1;
     //console.log(info);
         
     //console.log(parentSelect);
     for (let key in info){
         //console.log(info[key].id);
-        let parentSelect = document.querySelector('.row-data');
-        //.log(parentSelect);
-        let createSelect = document.createElement('div');
+        
+        
+        if (count <4){
+
+            //Adding Column
+
+            let parentSelect = document.querySelector("." + CSS.escape(rowId) + "");
+        //let parentSelect = document.querySelector('.rowId1');  
+        //console.log(parentSelect);      
+        let createSelectCol = document.createElement('div');
         let backdropPath="";
         let posterPath="";
-
-        if (info[key].backdrop_path == null){
+            if (info[key].backdrop_path == null){
             
-        }
-        else{
-            backdropPath='<center><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+info[key].backdrop_path+'" class="img-thumbnail" alt="..." style="max-height:300px;"></center><br>';
-        }
-        if (info[key].poster_path == null){
-            
-        }
-        else{
-            posterPath='<center><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+info[key].poster_path+'" class="img-thumbnail" alt="..." style="max-height:300px;"></center><br>';
-        }
-        if (count <3){
-        createSelect.innerHTML = `
+            }
+            else{
+                backdropPath='<center><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+info[key].backdrop_path+'" class="img-thumbnail" alt="..." style="max-height:300px;"></center><br>';
+            }
+            if (info[key].poster_path == null){
+                
+            }
+            else{
+                posterPath='<center><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+info[key].poster_path+'" class="img-thumbnail" alt="..." style="max-height:300px;"></center><br>';
+            }
+            colId='colId' + rowCount + count;
+        createSelectCol.innerHTML = `
         ${backdropPath}
         ${posterPath}
         <b>Key:</b>${info[key].adult}<br>
@@ -42,28 +52,82 @@ export const showMovie = async() => {
         <b>Vote count:</b>${info[key].vote_count}<br>     
         `
         ;
-        createSelect.classList.add('col-4');
-        createSelect.classList.add('border');
-        parentSelect.appendChild(createSelect);
+        createSelectCol.classList.add('col-4');
+        createSelectCol.classList.add('border');
+        createSelectCol.classList.add(colId);
+        parentSelect.appendChild(createSelectCol);
         count=count+1;
         }
         else{
+
+//Adding Row
+
             let parentSelect = document.querySelector('.movie-data');
-            let createSelect = document.createElement('div'); 
-            count=0;
-            createSelect.innerHTML = `
+            let createSelectRow = document.createElement('div'); 
+            count=1;
+            createSelectRow.innerHTML = `
        
         `
         ;
-        createSelect.classList.add('row');
-        createSelect.classList.add('row-data');
-        parentSelect.appendChild(createSelect);
-
+        rowCount=rowCount+1;
+        rowId='rowId'+ rowCount;
+        createSelectRow.classList.add('row');
+        createSelectRow.classList.add('row-data');
+        createSelectRow.classList.add(rowId);
+        parentSelect.appendChild(createSelectRow);
+        
+        //Adding Column
+         
+        parentSelect = document.querySelector("." + CSS.escape(rowId) + "");
+        //let parentSelect = document.querySelector('.rowId1');  
+        //console.log(parentSelect);      
+        let createSelectCol = document.createElement('div');
+        let backdropPath="";
+        let posterPath="";
+        if (info[key].backdrop_path == null){
+            
+        }
+        else{
+            backdropPath='<center><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+info[key].backdrop_path+'" class="img-thumbnail" alt="..." style="max-height:300px;"></center><br>';
+        }
+        if (info[key].poster_path == null){
+            
+        }
+        else{
+            posterPath='<center><img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/'+info[key].poster_path+'" class="img-thumbnail" alt="..." style="max-height:300px;"></center><br>';
+        }
+        colId='colId' + rowCount + count;
+    createSelectCol.innerHTML = `
+    ${backdropPath}
+    ${posterPath}
+    <b>Key:</b>${info[key].adult}<br>
+    <b>Language:</b>${info[key].original_language}<br>
+    <b>Original title:</b>${info[key].original_title}<br>
+    <b>Overview:</b>${info[key].overview}<br>
+    <b>Popularity:</b>${info[key].popularity}<br>
+    <b>Release date:</b>${info[key].release_date}<br>
+    <b>Title:</b>${info[key].title}<br>
+    <b>Video:</b>${info[key].video}<br>
+    <b>Vote average:</b>${info[key].vote_average}<br>
+    <b>Vote count:</b>${info[key].vote_count}<br>     
+    `
+    ;
+    createSelectCol.classList.add('col-4');
+    createSelectCol.classList.add('border');
+    createSelectCol.classList.add(colId);
+    parentSelect.appendChild(createSelectCol);
+    count=count+1;
+        
+        
         }
         
 
     }
-    
+    const asd = async() => {
+        console.log('printed');
+    }
+
+  
 
 
 }
